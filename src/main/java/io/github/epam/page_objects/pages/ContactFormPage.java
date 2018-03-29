493c3d5f9bfc383f4100d1de62050ef2af2fa86a
Catchermax <Catchermax@gmail.com>
2018-03-29 13:15:27 +0300

    add BaseDriverClass ready add ContactFormpage inProgress

--- src/main/java/io/github/epam/page_objects/pages/ContactFormPage.java
+++ src/main/java/io/github/epam/page_objects/pages/ContactFormPage.java
@@ -1,7 +1,11 @@
 package io.github.epam.page_objects.pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
+/*класс описывает страницу https://epam.github.io/JDI/contacts.html , только то что находится на этой странице, не включает в себя хедер, футор, блоки слева и справа*/
 
 public class ContactFormPage {
     WebDriver driver;
@@ -11,5 +15,12 @@ public class ContactFormPage {
     }
 
     //описание элементов
-    
+    @FindBy(css = "div.main-content-hg form#contact-form input#Name")
+    private WebElement nameField;
+
+
+    //действия с элементами на странице на уровке UI
+    public void nameFieldSendKeys(String name){//ввод в форму имени
+        nameField.sendKeys(name);
+    }
 }
